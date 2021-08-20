e27aa5f04f26b987f608ef7bc9c60ae7372acd3c
nihaltiwari <nihaltiwari28@gmail.com>
2021-08-20 10:01:41 +0530

    comming soon page

--- src/Routes.js
+++ src/Routes.js
@@ -17,22 +17,24 @@ import Paymentsuccess from "./components/Paymentsuccess";
 import PrivacyPolicy from "./components/PrivacyPolicy";
 import Allproductlist from "./components/Allproductlist";
 import Checkoutlogin from "./components/Checkoutlogin";
+import Comingsoon from "./components/Comingsoon";
 
 function Routes() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact>
-          <Header />
+          <Comingsoon />
+          {/* <Header />
           <Navigation />
           <Hero />
           {/* <Productrow /> */}
-          <Categorybanner />
+          {/* <Categorybanner />
           <Newproducts />
           <Bottombanner />
-          <Footer />
+          <Footer /> */}
         </Route>
-        <Route path="/productDetails/:id" exact component={Producdetails} />
+        {/* <Route path="/productDetails/:id" exact component={Producdetails} />
         <Route path="/allProducts/:category" exact component={Allproductlist} />
         <Route path="/shop/:subCategory" exact component={Shop} />
         <Route path="/cart" exact component={Shoppingcart} />
@@ -40,7 +42,7 @@ function Routes() {
         <Route path="/payment/:id" exact component={Paymentpage} />
         <Route path="/payment-success" exact component={Paymentsuccess} />
         <Route path="/privacyPolicy" exact component={PrivacyPolicy} />
-        <Route path="/login" exact component={Checkoutlogin} />
+        <Route path="/login" exact component={Checkoutlogin} /> */}
       </Switch>
     </Router>
   );
---
+++ src/components/Comingsoon.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Comingsoon() {
+  return (
+    <div>
+      <div>
+        <p style={{ textAlign: "center", marginTop: "10%" }}>
+          <img src="/assets/images/home-cosmetics/pop-logo.png" alt="logo" />
+        </p>
+      </div>
+      <h1 style={{ textAlign: "center", marginTop: "4rem", fontSize: "3rem" }}>
+        Comin<span style={{ color: "#52CAF2" }}>g</span>{" "}
+        <span style={{ color: "#52CAF2" }}>S</span>oon .....
+      </h1>
+    </div>
+  );
+}
+
+export default Comingsoon;
