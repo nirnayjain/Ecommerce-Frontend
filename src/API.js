0ef3bcf9dad0d38f18133f86eae963a21fe65f1c
Nirnay Jain <nirujainofficial@gmail.com>
2021-09-21 14:10:08 +0530

    url updated

--- src/API.js
+++ src/API.js
@@ -1,3 +1,3 @@
 //export const API = "https://popstoreeee.herokuapp.com";
-export const API = "http://localhost:5000";
-// export const API="https://api.potionsofparadise.com"
+//export const API = "http://localhost:5000";
+export const API="https://api.potionsofparadise.com"
