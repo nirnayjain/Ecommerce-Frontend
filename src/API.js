8ea2968d23bd0628bded63e40cc1b19371f27b11
yogita-taluja <yogita.taluja21@gmail.com>
2021-09-28 20:54:26 +0530

    slide

--- src/API.js
+++ src/API.js
@@ -1,8 +1,8 @@
 export const API = "https://popstoreeee.herokuapp.com";
-<<<<<<< HEAD
+
 // export const API = "http://localhost:3000";
-=======
+
 
 //export const API = "http://localhost:3000";
->>>>>>> 50ed584531bb2d8fabea87351fa1277ed0fb9de0
+
 //export const API="https://api.potionsofparadise.com"
