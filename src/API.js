ecdef08c20f5d1b8f0c084b5147e924105b63b0b
Nirnay Jain <nirujainofficial@gmail.com>
2022-05-16 17:00:25 +0530

    api changed

--- src/API.js
+++ src/API.js
@@ -1,8 +1,8 @@
-//export const API = "https://popstoreeee.herokuapp.com";
+export const API = "https://shopindia-backend.herokuapp.com";
 
  //export const API = "http://localhost:3000";
 
 
-export const API = "http://localhost:3000";
+//export const API = "http://localhost:3000";
 
 //export const API="https://api.potionsofparadise.com"
